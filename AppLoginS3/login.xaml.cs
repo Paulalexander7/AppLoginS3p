afa5b5555164d37c78b13c0850e5d2e99fb39122
PC <PC@DESKTOP-GF3EBS0>
2023-01-03 13:48:51 -0500

    Agregar archivos de proyecto.

---
+++ AppLoginS3/login.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppLoginS3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class login : ContentPage
+    {
+        public login()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            String sUsuario = txtUsuario.Text;
+            String sContraseña = txtContraseña.Text;
+
+            if((sUsuario == "estudiante2022" ) && (sContraseña == "uisrael2022"))
+            {
+                Navigation.PushAsync(new Dashboard());
+
+            }
+            else
+            {
+                lblResultado.Text = "El usuario o contraseña es incorrecto";
+            }
+    }
+}
+}
